cd5e2ada80c4e202e9373f2d3f2ca98843121538
manikanta-python-learner <kantamani2813@gmail.com>
2026-06-08 21:00:38 +0530

    Add files via upload

---
+++ # Assignment operators and comparison operators  3.py
@@ -0,0 +1,73 @@
+# Assignment Operators with Tough Numbers
+
+# 1. Add and Assign (+=)
+a = 987654321
+b = 123456789
+a += b
+print("1. Add and Assign (a += b) :", a)
+
+# 2. Subtract and Assign (-=)
+a = 876543210
+b = 234567890
+a -= b
+print("2. Subtract and Assign (a -= b) :", a)
+
+# 3. Multiply and Assign (*=)
+a = 12345
+b = 6789
+a *= b
+print("3. Multiply and Assign (a *= b) :", a)
+
+# 4. Divide and Assign (/=)
+a = 987654321
+b = 12345
+a /= b
+print("4. Divide and Assign (a /= b) :", a)
+
+# 5. Modulus and Assign (%=)
+a = 987654321
+b = 12345
+a %= b
+print("5. Modulus and Assign (a %= b) :", a)
+
+# 6. Floor Division and Assign (//=)
+a = 987654
+b = 12345
+a //= b
+print("6. Floor Division and Assign (a //= b) :", a)
+
+# 7. Exponentiation and Assign (**=)
+a = 155465
+b = 8216546
+a **= b
+print("7. Exponentiation and Assign (a **= b) :", a)
+print ("\nNow learning comparision operators *****:")
+       
+
+# comparision operators *****
+
+# 1) Equal to (==) check two values ae Equal or Not 
+a=125
+b=202
+c=231
+print("The values are Equal:",a==b)
+print("The values are Equal:",c==b)
+
+# 2)  (!=) check two values are Not Equal 
+a=54
+b=20
+c=20
+print("The values are  Not Equal:",a!=b)
+print("The values are  Not Equal:",c!=b)
+
+# 3) > greater than  and Less then 
+a=10
+b=24
+print ("a is Greater than b:",a>b)
+print ("a is Greater than b:",a<b)
+
+#4) <= (less then  or equal to ) and >= (Greter then or equal to )
+a=23
+b=254
+print ("Greater than or equal to :",a>=b)
+print ("less than or wqual to :",a<=b)
